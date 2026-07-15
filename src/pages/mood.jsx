21354638bb70dc20d6b6a566e095977a5524a6b9
risqy <mhadrisqy@gmail.com>
2026-07-15 22:32:02 +0700

    feat/add little tailwind css

--- src/pages/mood.jsx
+++ src/pages/mood.jsx
@@ -36,8 +36,21 @@ const Mood = () => {
 
       <h1>Sleep History</h1>
       <h3>Total duration last night</h3>
-      <input type="range" id="sleepDuration" name="sleepDuration" min="0" max="720" step="1" 
-      onChange={(e) => setSleepDuration(parseInt(e.target.value))} value={sleepDuration} />
+      <div className="my-8">
+      <input 
+      type="range" 
+      id="sleepDuration" 
+      name="sleepDuration" 
+      min="0" 
+      max="720" 
+      step="1" 
+      onChange={(e) => setSleepDuration(parseInt(e.target.value))} value={sleepDuration} 
+      className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer accent-[#4F6F52]" />
+      <div className="flex justify-between text-xs text-gray-400 mt-2">
+            <span>0h</span>
+            <span>12h</span>
+          </div>
+        </div>
       <p>
         {Math.floor(sleepDuration / 60)} hours {sleepDuration % 60} minutes
       </p>
